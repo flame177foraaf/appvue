398ead1e3702a5c83398d3285aa0059a0617ba60
플렝미 <forflame0@gmail.com>
2020-11-20 11:38:47 +0900

    no message

--- server.js
+++ server.js
@@ -9,5 +9,6 @@ app.get(/.*/, function (req, res) {
  res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 const port = process.env.PORT || 8080
+
 app.listen(port)
 console.log(`site is listening on port: ${port}`)
